a6c45132ef8852acf444eba45ba99e8c1dbe9315
Diana Gakuya <deegakuya@gmail.com>
2023-09-19 22:39:42 +0300

    oj

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance) {
   // Write your code here!
+  let response;
+  if (rideDistance <=400) {
+    response = "This one is on me!";
+  }
+  if (rideDistance > 2000) {
+    response = "i will gladly take your thirty bucks.";
+  }
+
+if (rideDistance > 2500) {
+  response = "No can do.";
+}
+return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+//else statement for city selection
+function ternaryCheckCity(cityName) {
+  let feedback;
+  if (cityName === "NYC") {
+    feedback= "ok, sounds good.";
+  } else {
+    feedback = "no go.";
+  }
+  return feedback;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Switch case for tips
+function switchOncharmFromTip(tipAmount) {
+let reply;
+switch (tipAmount) {
+  case "generous":
+    reply = "Thank you so much.";
+    break;
+    case "not as generous":
+      reply = "Thank you.";
+      break;
+      default:
+        reply = "Bye.";
 }
+return reply;
+}
+
